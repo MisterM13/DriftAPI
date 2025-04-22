24ce4579f946808fa63b7ad1b3b2b587651f0199
Matthias Mueller <matthias95@me.com>
2025-04-22 17:44:45 +0200

    initial commit

---
+++ driftAPI.py
@@ -0,0 +1,205 @@
+################################################################
+#                                                              #
+#         driftAPI by MisterM (and a bit from chatGPT)         #
+#                                                              #
+#         Python script to receive data logs from the          #
+#         Sturmkind Drift APP via the comunity api interface   #
+#         and log the data into a Mysql Database.              #
+#                                                              #
+#         !!! Important:  !!!                                  #
+#         To set up and run, please read the readme.md         #
+#                                                              #
+################################################################
+
+
+#!/usr/bin/env python3
+from fastapi import FastAPI
+from fastapi import Request
+import pymysql
+from dotenv import load_dotenv
+from dataclasses import dataclass, asdict
+import os
+
+load_dotenv()
+
+
+#from chatGPT ---- making a class to query the database more easy
+@dataclass
+class Race:
+	game_id: str = None
+	user_name: str = None
+	event: str = None
+	time: str = None
+	target_code: int = None
+	false_start: bool = None
+	driven_distance: int = None
+	driven_time: int = None
+	score: int = None
+	orientations: list = None
+	total_score: int = None
+	total_driven_distance: int = None
+	total_driven_time: int = None
+	
+	def to_db_tuple(self):
+		return (
+			self.game_id,
+			self.user_name,
+			self.event,
+			self.time,
+			self.target_code,
+			self.false_start,
+			self.driven_distance,
+			self.driven_time,
+			self.score,
+			self.orientations,
+			self.total_score,
+			self.total_driven_distance,
+			self.total_driven_time,
+		)
+
+#Query to insert the data
+sql = """
+	INSERT INTO race (
+		game_id, user_name, `event`, `time`, target_code, false_start,
+		driven_distance, driven_time, score, orientations,
+		total_score, total_driven_distance, total_driven_time
+	) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+"""
+
+#----------
+
+
+conn = pymysql.connect(
+	host=os.getenv("DB_HOST"),
+	user=os.getenv("DB_USER"),
+	password=os.getenv("DB_PASSWORD"),
+	database=os.getenv("DB_NAME")
+)
+cursor = conn.cursor()
+
+#games = [] #TODO: implement handling of multiple games
+users = []
+users_n = []
+user_times =[] # (user,time,flag) flag: s= start, r=round, f=finish
+cars = []
+total_laps = 5
+lap_counts = []
+false_starts=[]
+scores = []
+game_id = "Test1"
+
+spacer="------------------------------------------------------------------------"
+
+def printStats():
+	print("User","Car","false_start","laps","/","total_laps","score")
+	for i in range(len(users_n)):
+		print(users_n[i],cars[i],false_starts[i],lap_counts[i],"/",total_laps,scores[i])
+
+#converting the time format to match the datetime(3) format of the db
+def extractTime(time):
+	database_time = time.split("T")[0]+" "+time.split("T")[1].split("Z")[0]
+	return database_time
+	
+
+app = FastAPI()
+
+def check(game_id):
+	return game_id=="Test1"
+
+@app.get("/")
+def read_root():
+	return {"message": "Hello, HTTPS world!"}
+
+# Connection through ping
+@app.get("/game/{game_id}/ping")
+def getping(game_id):
+	#check(game_id)
+	return {"status":'true',"start_time":'null',"start_delay": 'null',"lap_count": 'null',"track_condition": 'null',"track_bundle": 'null',"wheels": 'null',"setup_mode": 'null'} #TODO: get infos from stats method/file
+
+#Enter the race
+@app.post("/game/{game_id}/enter")
+async def getEnter(game_id,request: Request):
+	#check(game_id)
+	data = await request.json()
+	game_id = data.get("game_id")
+	user_id = data.get("user_id")
+	user_name = data.get("user_name")
+	car_name = data.get("data").get("car_name")
+	if user_id and user_name is not None:
+		users.append(user_id)
+		users_n.append(user_name)
+		if car_name is not None:
+			cars.append(car_name)
+		else:
+			cars.append("unknown car")
+		lap_counts.append(0)
+		scores.append(0)
+		false_starts.append(False)
+		print(spacer,"Added new User:",user_name)
+		return "OK"
+
+#Start the race		
+@app.post("/game/{game_id}/start")
+async def getStart(game_id,request: Request):
+	#check(game_id)
+	data = await request.json()
+	user_name = data.get("user_name")
+	time = data.get("data").get("signal_time")
+	user_times.append((user_name,time,"s"))
+	print(spacer,user_name,"started the race")
+	race = Race(game_id=game_id, user_name=user_name, event="start",time=extractTime(time))
+	cursor.execute(sql, race.to_db_tuple())
+	conn.commit()
+	printStats() 
+	return "OK"
+
+#Cross a target
+@app.post("/game/{game_id}/target")
+async def getTarget(game_id,request: Request):
+	#check(game_id)
+	data = await request.json()
+	user_name = data.get("user_name")
+	time = data.get("data").get("crossing_time")
+	user_times.append((user_name,time,"r"))
+	code = data.get("data").get("target_code")
+	false_start=data.get("data").get("false_start")
+	dd = data.get("data").get("driven_distance")
+	dt = data.get("data").get("driven_time")
+	orientations =data.get("data").get("orientations") #TODO: have to sort out some orientations format Problem.
+	for i in range(0,len(users_n)):
+		if users_n[i]==user_name:
+			false_starts[i]=false_start
+			scores[i] += data.get("data").get("score")
+			#print(scores[i])
+			lap_counts[i]+=1
+			race = Race(game_id=game_id, user_name=user_name, event="target",target_code=code,false_start=false_start, time=extractTime(time),driven_distance=dd,driven_time=dt)
+			cursor.execute(sql, race.to_db_tuple())
+			conn.commit()
+	printStats() 
+	print(spacer,user_name,"finished a round. Round Time:",time)
+	return "OK"
+
+
+#Finished the race
+@app.post("/game/{game_id}/end")
+async def getEnd(game_id,request: Request):
+	#check(game_id)
+	data = await request.json()
+	user_name = data.get("user_name")
+	time = data.get("data").get("finished_time")
+	user_times.append((user_name,time,"f"))
+	tot_score = data.get("data").get("total_score")
+	tot_dd = data.get("data").get("total_driven_distance")
+	tot_dt = data.get("data").get("total_driven_time")
+	race = Race(game_id=game_id, user_name=user_name, event="end",time=extractTime(time),total_score=tot_score,total_driven_distance=tot_dd,total_driven_time=tot_dt)
+	cursor.execute(sql, race.to_db_tuple())
+	conn.commit()
+	print(spacer,user_name,"finished the race")
+	for i in range(0,len(users_n)):
+		if users_n[i]==user_name:
+			if scores[i]!=tot_score:
+				#print(scores[i])
+				print(spacer,"total score of",user_name,"did not equal added up score")
+				scores[i]=tot_score
+	printStats() 
+	return "OK"
---
+++ readme.md
@@ -0,0 +1,62 @@
+﻿
+
+
+> Written with [StackEdit](https://stackedit.io/) 
+
+# driftAPI Script
+
+### About:
+This script was build to easily receive the data from the Dr!ft App over the Drift Comunity API and log them into an MySQL database.
+
+### Setup:
+
+1. Create a [MySQL](https://www.mysql.com) Database.
+2. Create following table with an sql query:
+```sql
+create Table race (log_id INT AUTO_INCREMENT PRIMARY KEY, game_id varchar(50) not null, user_name varchar(50) not null,  `event`  varchar(50),`time`  datetime, target_code int, false_start bool, driven_distance int, driven_time int, score int, orientations json, total_score int, total_driven_distance int, total_driven_time int );
+```
+3. Create an .env File inside the Project Folder with the access data to the Database:
+```
+DB_HOST=<hostadress>
+DB_USER=<database username>
+DB_PASSWORD=<password>
+DB_NAME=<database name>
+ ```
+4. Open a Terminal from the Project Folder and install the Python requirements with:
+`pip install -r requirements.txt`
+5. Open a Terminal from the Project Folder and run: `ngrok http 8001`
+6. Open a Terminal from the Project Folder and run: `uvicorn driftAPI:app --host 0.0.0.0 --port 8001`
+On success it displays some infos and: `Uvicorn running on http://0.0.0.0:8001 (Press CTRL+C to quit)`
+7. Now open the Dr!ft App -> Play ->Race -> Community API on:
+```
+Username: <your name>
+URL: <https://...ngrok-free.app/game>
+Game ID: <Test1>
+```
+If there's a green tick in the App, your're all good.
+
+#### With self signed Certificates without ngrok
+You can also try to run the Script with self signed Certificates. However I couldn't manage to run it, because the App probably don't accept self signed certificates. However if you want to try:
+1. Open a Terminal from the Project Folder and Generate the Keys with:
+`openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365 -nodes`
+2. Run the script with:
+`uvicorn driftAPI:app --host 0.0.0.0 --port 8001 --ssl-keyfile=key.pem --ssl-certfile=cert.pem`
+
+### Limitations:
+
+- [ ] no settings for the cars/races yet
+- [ ] handles only one race at the time
+- [ ] no calculations of the lap durations yet
+- [x] one instance to collect all the data from start, target and end
+- [ ] problem with collecting the orientations data
+- [x] on time status printouts
+- [x] logging the stats into a database   
+- [ ] no webui 
+- [ ] no app
+- [ ] no csv export
+
+
+### License
+This Project is free to use and modify under the [GPL](https://www.gnu.org/licenses/gpl-3.0.html).
+ Have fun with your cars ;-)
+ 
